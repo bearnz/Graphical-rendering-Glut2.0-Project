7544cfd33623ff53c9c810333fc5472ac222ebe7
raebeht <38020104+raebeht@users.noreply.github.com>
2020-08-11 14:28:01 +1200

    Uploaded CMakeFiles folder with dependencies

---
+++ CMakeFiles/museum.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/museum.out.dir/museum.cpp.o"
+  "museum.out.pdb"
+  "museum.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/museum.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
